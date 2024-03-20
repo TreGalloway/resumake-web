1e73a3ee3714301242f0732f441745306c142533
Tre <tregallowayofficial@gmail.com>
2024-03-20 15:47:32 -0500

    Add FloatingNav component and update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { TypewriterEffectDemo } from "../components/typewriter";
 import { BentoGridThirdDemo } from "../components/bento";
+import { MacbookScroll } from "../components/ui/macbook";
+import { FloatingNavDemo } from "../components/nav";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <FloatingNavDemo />
       <TypewriterEffectDemo />
       <BentoGridThirdDemo />
+      <MacbookScroll />
     </main>
   );
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import { FloatingNav } from "./ui/floating-nav";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+export function FloatingNavDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="w-4 h-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="w-4 h-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <IconMessage className="w-4 h-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
+  return (
+    <div className="relative w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
+}
+
