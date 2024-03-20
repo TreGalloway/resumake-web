12ceaf04289e47d2592b7e6265172988681dfea7
Tre Galloway <88118603+TreGalloway@users.noreply.github.com>
2024-03-19 23:03:32 -0500

    starting resumake webapp

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["pbs.twimg.com"],
+        // add more domain names if needed
+      },
+    
+};
 
 export default nextConfig;
